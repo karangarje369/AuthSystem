454594225ac572d43673adf0f719cabef7c743bb
Karan Garje <karangarje098@gmail.com>
2026-06-17 21:38:59 +0530

    ababab

--- Frontend/auth/src/api/authApi.js
+++ Frontend/auth/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 let URI = import.meta.env.VITE_URI
 const API = axios.create(
-{    baseURL:{URI},
+{    baseURL:URI,
 })
 
 export const registerAPI = (userData) => API.post("/register",userData);
